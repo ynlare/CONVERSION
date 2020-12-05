6d002f1cb0e92e20f312cdc32b6de4554046a203
ynlare <yedou92@gmail.com>
2020-12-05 13:18:20 -0600

    work on calculation
    work on calculation

--- JAVASCRIPT/temp.js
+++ JAVASCRIPT/temp.js
@@ -1,18 +1,2 @@
-var celsius= document.getElementById("celsius") 
-
-var fahrenheit= document.getElementById("fahrenheit") 
-
-var result= document.querySelector("div.result") 
-
-if(celsius.Value){
-
-    fahrenheit.Value=celsius.Value*9/5+32;
-
-    result.innerHTML=celsius.Value+ "°C = " + fahrenheit.Value+ "°F"}
 
 
-
-
-
-
-}
